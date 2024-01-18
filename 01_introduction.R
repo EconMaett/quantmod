4c6b914f7620b5cd59ffefffb66112275fe4e07c
EconMaett <matthias.spichiger@bluewin.ch>
2024-01-18 12:59:19 +0100

    Initial commit

---
+++ 01_introduction.R
@@ -0,0 +1,11 @@
+# quantomod ----
+
+# Quantitative Financial Modelling & Trading Framework for R 
+
+# Website: http://www.quantmod.com/
+
+# quantmod_0.4.25 on CRAN
+# https://blog.fosstrading.com/2023/08/quantmod-0-4-25-on-cran/
+
+# GitHub: https://github.com/joshuaulrich/quantmod
+
